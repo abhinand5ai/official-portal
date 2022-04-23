3b87288204426f044a8e8e83e5128f199526c391
abhinand <abhinandsainasari@gmail.com>
2022-04-23 13:42:20 -0500

    es lint fix for clients

--- server/index.js
+++ server/index.js
@@ -1,22 +1,23 @@
 // server/index.js
-import { dirname, join, resolve } from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
-import 'dotenv/config';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from "mongoose";
-import eventsRouter from './routes/events.js';
 
-const app = express();
 
-app.use(bodyParser.json({ limit: '50mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+const app = require('express')();
+const path = require('path');
+const mongoose = require('mongoose');
+const url = require('url');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const eventsRouter = require('./routes/events.js');
+
+require('dotenv').config();
+
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(__dirname);
-console.log(resolve(__dirname, '../client/build'));
+const dirname = path.dirname(url.fileURLToPath(require("meta").url));
+console.log(dirname);
+console.log(path.resolve(dirname, '../client/build'));
 // Have Node serve the files for our built React app
 app.use(express.static(resolve(__dirname, '../client/build')));
 
@@ -25,7 +26,7 @@ app.use('/api/events', eventsRouter);
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(resolve(__dirname, '../client/build', 'index.html'));
 });
 
 
@@ -33,14 +34,14 @@ const CONNECT_URL = process.env.MONGODB_CONNECTION_STRING;
 const PORT = process.env.PORT || 5000;
 
 
-
-try {
-  await mongoose.connect(CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-} catch (err) {
-  console.log("Error connecting to MongoDB");
-  console.log(CONNECT_URL);
-}
+mongoose.connect(CONNECT_URL,
+    {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    }).error(err => {
+        console.log(err);
+        console.log('Error connecting to MongoDB');
+        console.log(CONNECT_URL);
+    });
